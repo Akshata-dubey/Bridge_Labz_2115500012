84e88eaf7786ddc58ec1f81e70d952a57b6d22ce
Akshata Dubey <akshatadubey213@gmail.com>
2025-01-20 01:00:58 +0530

    q1

---
+++ ques1.cs
@@ -0,0 +1,10 @@
+Q1. Write a program that prints "Welcome to Bridgelabz!" to the screen.
+
+Ans     using System;
+class WelcomeProgram
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to Bridgelabz!");
+    }
+}
