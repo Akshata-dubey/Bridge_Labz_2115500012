cb134a7db646b93ab52e93d9ed4b1f1ad499eb9a
Akshata Dubey <akshatadubey213@gmail.com>
2025-01-20 01:15:32 +0530

    q7

---
+++ ques7.cs
@@ -0,0 +1,17 @@
+Q7. Write a program to calculate the perimeter of a rectangle. Take the length
+and width as inputs and use the formula:
+Perimeter = 2 * (length + width).
+Ans
+using System;
+class RectanglePerimeter
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter the length of the rectangle: ");
+        double length = double.Parse(Console.ReadLine());
+        Console.Write("Enter the width of the rectangle: ");
+        double width = double.Parse(Console.ReadLine());
+        double perimeter = 2 * (length + width);
+        Console.WriteLine($"The perimeter of the rectangle is: {perimeter}");
+    }
+}
